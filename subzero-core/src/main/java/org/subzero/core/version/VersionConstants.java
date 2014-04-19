c540a5979999affd25135b6bf90407252267d556
ahuh <ahuh@free.fr>
2014-04-19 14:27:33 +0200

    Prepare next version snapshot

--- subzero-core/src/main/java/org/subzero/core/version/VersionConstants.java
+++ subzero-core/src/main/java/org/subzero/core/version/VersionConstants.java
@@ -10,10 +10,10 @@ public class VersionConstants {
 	/**
 	 * Version number
 	 */
-	public static final String SUBZERO_VERSION_NUMBER = "v1.0.5";
+	public static final String SUBZERO_VERSION_NUMBER = "v1.0.6";
 	
 	/**
 	 * Version date
 	 */
-	public static final String SUBZERO_VERSION_DATE = "2014-04-19";
+	public static final String SUBZERO_VERSION_DATE = "2014-XX-XX";
 }
